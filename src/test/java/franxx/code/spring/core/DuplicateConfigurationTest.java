46ab7ed807bd2e5a38d3becd4285beb9c9403549
Hilmi AM <imiia75775@proton.me>
2024-02-28 19:45:56 +0700

    Duplicate Bean

---
+++ src/test/java/franxx/code/spring/core/DuplicateConfigurationTest.java
@@ -0,0 +1,32 @@
+package franxx.code.spring.core;
+
+import franxx.code.spring.core.data.Foo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DuplicateConfigurationTest {
+
+    @Test
+    void duplicate() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DuplicateConfiguration.class);
+
+        assertThrows(NoUniqueBeanDefinitionException.class, () -> {
+            Foo foo = context.getBean(Foo.class);
+        });
+    }
+
+    @Test
+    void getBean() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DuplicateConfiguration.class);
+
+        Foo foo1 = context.getBean("foo1", Foo.class);
+        Foo foo2 = context.getBean("foo2", Foo.class);
+
+        assertNotSame(foo1, foo2);
+    }
+
+}
