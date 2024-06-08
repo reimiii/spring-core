001d5eedee1a21b1b2eeb7bf7170ada2591c2593
Hilmi AM <imiia75775@proton.me>
2024-06-08 22:43:27 +0700

    Depends-On

---
+++ src/main/java/franxx/code/spring/core/DependsOnConf.java
@@ -0,0 +1,26 @@
+package franxx.code.spring.core;
+
+import franxx.code.spring.core.data.Bar;
+import franxx.code.spring.core.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Slf4j
+@Configuration
+public class DependsOnConf {
+
+    @Bean
+    @DependsOn("bar")
+    public Foo foo() {
+        log.info("Creating Foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        log.info("Creating Bar");
+        return new Bar();
+    }
+}
