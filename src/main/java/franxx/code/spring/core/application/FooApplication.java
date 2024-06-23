f3ea64f1b9e1335e4d49ec0e18dc0201a93c60ee
Hilmi AM <imiia75775@proton.me>
2024-06-23 13:07:34 +0700

    Spring Boot Application

---
+++ src/main/java/franxx/code/spring/core/application/FooApplication.java
@@ -0,0 +1,22 @@
+package franxx.code.spring.core.application;
+
+import franxx.code.spring.core.data.Foo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FooApplication {
+
+    @Bean
+    public Foo foo() {
+        return new Foo();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+        Foo foo = applicationContext.getBean(Foo.class);
+        System.out.println(foo);
+    }
+}
