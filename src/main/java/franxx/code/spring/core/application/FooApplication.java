fb0d27219129a33924ed0fba3f111e9e6520c959
Hilmi AM <imiia75775@proton.me>
2024-06-23 13:59:00 +0700

    Spring-Application-Event

--- src/main/java/franxx/code/spring/core/application/FooApplication.java
+++ src/main/java/franxx/code/spring/core/application/FooApplication.java
@@ -2,11 +2,14 @@ package franxx.code.spring.core.application;
 
 import franxx.code.spring.core.data.Bar;
 import franxx.code.spring.core.data.Foo;
+import franxx.code.spring.core.listener.AppStartingListener;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FooApplication {
@@ -24,7 +27,10 @@ public class FooApplication {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(FooApplication.class);
-        application.setBannerMode(Banner.Mode.OFF);
+//        application.setBannerMode(Banner.Mode.OFF);
+        application.setListeners(List.of(
+                new AppStartingListener()
+        ));
         ConfigurableApplicationContext context = application.run(args);
 
         Foo foo = context.getBean(Foo.class);
