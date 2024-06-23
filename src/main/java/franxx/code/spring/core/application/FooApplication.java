d04695901e0906740d933888835b63d69afc15a4
Hilmi AM <imiia75775@proton.me>
2024-06-23 13:42:43 +0700

    Banner

--- src/main/java/franxx/code/spring/core/application/FooApplication.java
+++ src/main/java/franxx/code/spring/core/application/FooApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 public class FooApplication {
 
     @Bean
-    public Foo foo(Bar bar) {
+    public Foo foo() {
         return new Foo();
     }
 
